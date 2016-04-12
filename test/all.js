8e4233f772130cde4a0928320991aab95cc8cfea
Sérgio Ramos <mail@sergioramos.me>
2016-04-12 20:05:15 +0100

    update dependencies

--- test/all.js
+++
@@ -1,4 +0,0 @@
-exports['test React completion'] = require('./completion/React');
-exports['test JSX completion'] = require('./completion/JSX');
-
-if (require.main === module) require("test").run(exports);
